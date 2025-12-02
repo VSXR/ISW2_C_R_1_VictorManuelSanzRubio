3a71d1621c48b103f18267938f5aa8ff8d7f81a7
Chete99 <7736035@alumnos.ufv.es>
2025-12-02 19:29:34 +0100

    Agrega test que verifica que el formulario InfoRequestForm es válido con datos correctos

--- relecloud/tests/test_forms.py
+++ relecloud/tests/test_forms.py
@@ -1,7 +1,21 @@
 from django.test import TestCase
+from django.core import mail
 from ..models import Cruise
+from ..forms import InfoRequestForm
 
 class FormTests(TestCase):
 
     def setUp(self):
         self.cruise = Cruise.objects.create(name="Test Cruise", desc="...")
+
+    def test_info_request_form_sends_email_on_save(self):
+        form_data = {
+            'name': 'Test User',
+            'email': 'test@example.com',
+            'phone': '123456789',
+            'message': 'This is a test message',
+            'cruise': self.cruise.pk
+        }
+
+        form = InfoRequestForm(data=form_data)
+        pass
